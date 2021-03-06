274d8edf8ffba0e7ea4e6c5aa9e8e7bc704eb838
Patryk Kolanek <marti3go@gmail.com>
2021-03-06 12:05:11 +0100

    Add properties

--- app/src/main/java/pl/lodz/zzpj/App.java
+++ app/src/main/java/pl/lodz/zzpj/App.java
@@ -2,9 +2,24 @@ package pl.lodz.zzpj;
 
 import pl.lodz.Client;
 
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.Properties;
+
 public class App {
 
     public static void main(String[] args) {
-        Client client = new Client("A", "B", 12);
+        Client client = new Client("A", "B", 18);
+        System.out.println(client);
+
+        String filePath = "config.properties";
+        Properties properties = new Properties();
+
+        try (InputStream resourceAsStream = App.class.getClassLoader().getResourceAsStream(filePath)) {
+            properties.load(resourceAsStream);
+            properties.forEach((k, v) -> System.out.println(k + ":" + v));
+        } catch (IOException e) {
+            System.err.println("Unable to load properties file : " + filePath);
+        }
     }
 }
