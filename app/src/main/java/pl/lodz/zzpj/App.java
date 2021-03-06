a6e0e31904d9f280f84717ca35ee64d4ce5c7de8
Patryk Kolanek <marti3go@gmail.com>
2021-03-06 09:55:28 +0100

    Add model-api and app which provide this model

---
+++ app/src/main/java/pl/lodz/zzpj/App.java
@@ -0,0 +1,10 @@
+package pl.lodz.zzpj;
+
+import pl.lodz.Client;
+
+public class App {
+
+    public static void main(String[] args) {
+        Client client = new Client("A", "B", 12);
+    }
+}
---
+++ model-api/src/main/java/pl/lodz/Client.java
@@ -0,0 +1,13 @@
+package pl.lodz;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+@Data
+@AllArgsConstructor
+public class Client {
+
+    private String name;
+    private String lastName;
+    private int age;
+}
